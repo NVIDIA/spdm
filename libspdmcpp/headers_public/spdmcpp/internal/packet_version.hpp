ab38c9fa0a811f0864e481538243db6fa80c5c99
Tomek Borowik <tomek@conclusive.pl>
2022-04-11 14:38:31 +0200

    refactor to avoid bitfields (cppcheck throws syntax errors?)

--- libspdmcpp/headers_public/spdmcpp/internal/packet_version.hpp
+++ libspdmcpp/headers_public/spdmcpp/internal/packet_version.hpp
@@ -5,22 +5,50 @@
 
 #ifdef SPDMCPP_PACKET_HPP
 
-struct PacketVersionNumber // TODO bitfields are ill-defined endianness-wise
-                           // iirc!?
+struct PacketVersionNumber
 {
-    uint16_t Alpha : 4 = 0;
-    uint16_t UpdateVersionNumber : 4 = 0;
-    uint16_t MinorVersion : 4 = 0;
-    uint16_t MajorVersion : 4 = 0;
+    uint16_t Bits = 0;
 
     static constexpr bool sizeIsConstant = true;
 
+    uint8_t getMajor() const
+    {
+        return Bits >> 12 & 0xF;
+    }
+    uint8_t getMinor() const
+    {
+        return Bits >> 8 & 0xF;
+    }
+    uint8_t setMinor() const
+    {
+        return Bits >> 8 & 0xF;
+    }
+    uint8_t getUpdateVersionNumber() const
+    {
+        return Bits >> 4 & 0xF;
+    }
+    uint8_t getAlpha() const
+    {
+        return Bits & 0xF;
+    }
+
+    void setMajor(uint8_t value)
+    {
+        Bits &= ~(0xF << 12);
+        Bits |= value << 12;
+    }
+    void setMinor(uint8_t value)
+    {
+        Bits &= ~(0xF << 8);
+        Bits |= value << 8;
+    }
+
     MessageVersionEnum getMessageVersion() const
     {
-        switch (MajorVersion)
+        switch (getMajor())
         {
             case 1:
-                switch (MinorVersion)
+                switch (getMinor())
                 {
                     case 0:
                         return MessageVersionEnum::SPDM_1_0;
@@ -34,26 +62,26 @@ struct PacketVersionNumber // TODO bitfields are ill-defined endianness-wise
     void print(LogClass& log) const
     {
         log.print("<");
-        SPDMCPP_LOG_expr(log, MajorVersion);
+        SPDMCPP_LOG_expr(log, getMajor());
         log.print("   ");
-        SPDMCPP_LOG_expr(log, MinorVersion);
+        SPDMCPP_LOG_expr(log, getMinor());
         log.print("   ");
-        SPDMCPP_LOG_expr(log, UpdateVersionNumber);
+        SPDMCPP_LOG_expr(log, getUpdateVersionNumber());
         log.print("   ");
-        SPDMCPP_LOG_expr(log, Alpha);
+        SPDMCPP_LOG_expr(log, getAlpha());
         log.print(">");
     }
 
     bool operator==(const PacketVersionNumber& other) const
     {
-        return memcmp(this, &other, sizeof(other)) == 0;
+        return Bits == other.Bits;
     }
 };
 
 inline void endianHostSpdmCopy(const PacketVersionNumber& src,
                                PacketVersionNumber& dst)
 {
-    dst = src; // TODO surely wrong
+    endianHostSpdmCopy(src.Bits, dst.Bits);
 }
 
 #endif
