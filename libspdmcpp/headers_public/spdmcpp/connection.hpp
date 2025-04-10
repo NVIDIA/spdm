a1b2a69c34691a4c71be1746318c38543be22b89
iagarwal <iagarwal@nvidia.com>
2025-04-10 12:11:12 +0530

    Fix timer retrigger issue in retry handling
    Fixes nvbug https://nvbugs/5200682
    
    A known GPU issue causes responder errors if the same packet
    is retried. Google 4.5 mins PERST sequence there will be a
    need for more get version attempts. We have total 4 request
    retries + initial request we will get total 5 mins.
    Also,when Retries are exhausted, return value is set to Ok,
    causing handleEvent to miss setting responder status to Error,
    leaving spdm daemon stuck in refresh state.
    
    Tests include various packet drop scenarios for certificates
    and commands, callback verification, SPDM sanity checks,
     and a reboot test with PERST injection.
    
    Signed-off-by: iagarwal <iagarwal@nvidia.com>

--- libspdmcpp/headers_public/spdmcpp/connection.hpp
+++ libspdmcpp/headers_public/spdmcpp/connection.hpp
@@ -821,7 +821,7 @@ class ConnectionClass : public NonCopyable
     /// Retry packet count
     uint8_t retryPktCount{};
 
-    static constexpr auto maxGetVersionRetries = 3U;
+    static constexpr auto maxGetVersionRetries = 6U;
     uint8_t retryGetVersionCount{};
 
     /// Response if ready token value
